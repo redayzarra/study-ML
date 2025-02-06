d3377dcff12d7d508d804a729eba5ff54adca019
ReDay Zarra <redayzarra@gmail.com>
2025-02-06 06:12:08 -0800

    Implementing gradient descent

--- Implementation/Gradient_Descent.py
+++ Implementation/Gradient_Descent.py
@@ -10,7 +10,7 @@ x_train = np.array([1.0, 2.0])
 y_train = np.array([300.0, 500.0]) 
 
 # Define a function to calculate the cost
-def calculate_cost(x: NDArray[np.float64], y: NDArray[np.float64], w: float, b: float) -> float:
+def compute_cost(x: NDArray[np.float64], y: NDArray[np.float64], w: float, b: float) -> float:
     # Get the number of training examples
     m = x.shape[0]
     cost = 0
@@ -67,3 +67,50 @@ def compute_gradient(x, y, w, b):
         
     return dj_dw, dj_db
 
+def gradient_descent(x, y, w_in, b_in, alpha, num_iters): 
+    """
+    Performs gradient descent to fit w,b. Updates w,b by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      x (ndarray (m,))  : Data, m examples 
+      y (ndarray (m,))  : target values
+      w_in,b_in (scalar): initial values of model parameters  
+      alpha (float):     Learning rate
+      num_iters (int):   number of iterations to run gradient descent
+      compute_cost:     function to call to produce cost
+      compute_gradient: function to call to produce gradient
+      
+    Returns:
+      w (scalar): Updated value of parameter after running gradient descent
+      b (scalar): Updated value of parameter after running gradient descent
+      J_history (List): History of cost values
+      p_history (list): History of parameters [w,b] 
+      """
+    
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    p_history = []
+    b = b_in
+    w = w_in
+    
+    for index in range(num_iters):
+        # Calculate the gradient and update the parameters using compute_gradient
+        dj_dw, dj_db = compute_gradient(x, y, w , b)     
+
+        # Update Parameters using equation (3) above
+        b = b - alpha * dj_db                            
+        w = w - alpha * dj_dw                            
+
+        # Save cost J at each iteration
+        if index < 100000:      # prevent resource exhaustion 
+            J_history.append(compute_cost(x, y, w , b))
+            p_history.append([w,b])
+            
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if index % math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {index:4}: Cost {J_history[-1]:0.2e} ",
+                  f"dj_dw: {dj_dw: 0.3e}, dj_db: {dj_db: 0.3e}  ",
+                  f"w: {w: 0.3e}, b:{b: 0.5e}")
+ 
+    return w, b, J_history, p_history #return w and J,w history for graphing
