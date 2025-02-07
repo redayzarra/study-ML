14f30996924061ce710bec78c9d93330b0e51148
ReDay Zarra <redayzarra@gmail.com>
2025-02-06 20:46:30 -0800

    Running gradient descent and plotting results

--- Implementation/Gradient_Descent.py
+++ Implementation/Gradient_Descent.py
@@ -114,3 +114,25 @@ def gradient_descent(x, y, w_in, b_in, alpha, num_iters):
                   f"w: {w: 0.3e}, b:{b: 0.5e}")
  
     return w, b, J_history, p_history #return w and J,w history for graphing
+
+# Initialize parameters
+w_init = 0
+b_init = 0
+
+# Add additional gradient descent settings
+iterations = 10000
+tmp_alpha = 1.0e-2
+
+# Run gradient descent
+w_final, b_final, J_history, p_history = gradient_descent(x_train ,y_train, w_init, b_init, tmp_alpha, iterations)
+print(f"(w,b) found by gradient descent: ({w_final:8.4f},{b_final:8.4f})")
+
+# Plot cost versus iteration  
+fig, (ax1, ax2) = plt.subplots(1, 2, constrained_layout=True, figsize=(12,4))
+ax1.plot(J_history[:100])
+ax2.plot(1000 + np.arange(len(J_history[1000:])), J_history[1000:])
+ax1.set_title("Cost vs. iteration(start)");  ax2.set_title("Cost vs. iteration (end)")
+ax1.set_ylabel('Cost')            ;  ax2.set_ylabel('Cost') 
+ax1.set_xlabel('iteration step')  ;  ax2.set_xlabel('iteration step') 
+plt.show()
+
