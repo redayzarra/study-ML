0323dc55852bda8a69692a68f4911e6ecc2e5c66
ReDay Zarra <redayzarra@gmail.com>
2025-04-13 23:32:16 -0700

    Plotting final predictions

--- Implementation/Polynomial_Regression.py
+++ Implementation/Polynomial_Regression.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-np.set_printoptions(precision=2)  # reduced display precision on numpy arrays
+np.set_printoptions(precision=2)
 
 # Create target data
 x = np.arange(0, 20, 1)
@@ -20,9 +20,9 @@ x = np.arange(0, 20, 1)
 y = 1 + x**2
 
 # Engineer features 
-X = x**2      #<-- added engineered feature
+X = x**2  
 
-X = X.reshape(-1, 1)  #X should be a 2-D Matrix
+X = X.reshape(-1, 1)  
 
 # Plot the data points
 plt.scatter(x, y, marker='x', c='r', label="Actual Value")
@@ -32,3 +32,18 @@ plt.ylabel("y")
 plt.legend()
 plt.show()
 
+# Create target data
+x = np.arange(0, 20, 1)
+y = 1 + x**2
+
+# Engineer features 
+X = x**2  
+X = X.reshape(-1, 1)  
+
+# Plot the data points
+plt.scatter(x, y, marker='x', c='r', label="Actual Value")
+plt.title("Added x**2 feature")
+plt.xlabel("x")
+plt.ylabel("y") 
+plt.legend()
+plt.show()
